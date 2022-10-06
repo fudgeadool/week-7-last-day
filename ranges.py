63e1cfa569c0bf105dd122d38f9dde028ada8239
Husam El <113930359+fudgeadool@users.noreply.github.com>
2022-10-06 17:25:09 +0000

    nigeria

--- ranges.py
+++ ranges.py
@@ -5,17 +5,17 @@ def ranges():
     # range
     # The range function allows you to quickly generate a list of integers, this comes in handy a lot, so take note of how to use it! There are 3 parameters you can pass, a start, a stop, and a step size. Let's see some examples:
   # instead of doing this:  
-    my_list = [0,1,2,3,4,5,6,7,8,9,10,11]
-  #you can do this instead
-    # print(list(range(0,78)))
-   my_list = list(range(0,78))
-   for number in my_list:
-     #number % 2 == 0 even
-     #number % 2 == 1 odd
-     if number % 2 == 0:
-      print(f"number {number} is even")
-     elif number % 2 == 1:
-      print(f"number {number} is odd")
+  #   my_list = [0,1,2,3,4,5,6,7,8,9,10,11]
+  # #you can do this instead
+  #   # print(list(range(0,78)))
+  #  my_list = list(range(0,78))
+  #  for number in my_list:
+  #    #number % 2 == 0 even
+  #    #number % 2 == 1 odd
+  #    if number % 2 == 0:
+  #     print(f"number {number} is even")
+  #    elif number % 2 == 1:
+  #     print(f"number {number} is odd")
     
 
 
@@ -32,26 +32,39 @@ def ranges():
   # step size just means how big of a jump/leap/step you 
   # take from the starting number to get to the next number.
   
-  # list(range(0,11,2))
+    # print(list(range(0,11,2)))
   
-  # list(range(0,101,10))
+    # print(list(range(0,101,10)))
   
   ##############################ranges#####################################################
   
   # Range Practice #1
   # Create a list consisting of all the numbers from 2500 to 2585 (inclusive). Store this list in the variable my_list.
   
-  
-  
+      
+my_list = list(range(2500,2585))
+print(list(range(2500,2585)))
   # Range Practice #2
   # Using the range() function, create in a single line of code a list consisting of all numbers that are multiples of 3 from 3 to 300 (inclusive). Store this list in the variable my_list.
   
-  
-  
+print(list(range(3,301, 3)))
   # Range Practice #3
   # Use the range() function and a loop to add the squares of all the numbers from 1 to 15 (inclusive). Store the result in a variable called sum_squares.
-  
-  
+sum_squares = 0
+for number in range(1,16):
+ sum_squares = sum_squares + number **2
+ print(sum_squares)
+
+
+  #add a range of numbers 1 - 150
+  #using a for loop
+  #store the result in a variable called sum
+
+sum = 0
+for number in range(1,151):
+  # sum = sum + number
+  sum += number
+print(sum)
   
   # For this purpose:
   
